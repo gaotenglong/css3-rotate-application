5ad092650906e261a402f504ae889b4527868402
gao <tenglong00@sina.com>
2016-07-25 15:51:35 +0800

    css3动画应用
    修改了图片暂停时停止旋转的位置

--- css3动画应用-音乐唱片旋转播放特效/myjs.js
+++ css3动画应用-音乐唱片旋转播放特效/myjs.js
@@ -8,16 +8,25 @@ window.onload=function(){
     var pic2 = document.getElementById("pic2");
     var xuanzhuan = document.getElementById("xuanzhuan");
     var audio = document.getElementsByTagName("audio")[0];
+    //当音乐播放完停止时，自动停止图片旋转
+    audio.addEventListener("ended",function(event){
+        xuanzhuan.setAttribute("id","");
+        //xuanzhuan.style.animationPlayState = "paused";
+    },false);
+    //点击暂停播放时，音乐暂停，图片停止旋转
     icon1.onclick=function(){
         audio.pause();
         pic1.style.display="none";
         pic2.style.display = "block";
-        xuanzhuan.setAttribute("id","");
+        //xuanzhuan.setAttribute("id","");
+        xuanzhuan.style.animationPlayState = "paused";
     };
+    //点击播放音乐时，音乐开始播放，图片开始旋转
     icon2.onclick=function(){
         audio.play();
         pic1.style.display="block";
         pic2.style.display = "none";
-        xuanzhuan.setAttribute("id","xuanzhuan");
+        //xuanzhuan.setAttribute("id","xuanzhuan");
+        xuanzhuan.style.animationPlayState = "running";
     }
 };
